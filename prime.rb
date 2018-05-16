0022f57692e2ba2a987a34de33b26d1e972a6d40
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-16 02:49:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,3 @@
-=begin
-def prime?(integer)
-  if integer % 2 === 0 && integer != 2
-    return false
-  elsif integer.between?(2, 5)
-    return true
-  elsif integer < 2
-    return false
-  end
-
-  check = 3
-  while check < integer
-    if integer % check === 0
-      return false
-    elsif check === integer / 2
-      return true
-    end
-    check += 1
-  end
-end
-=end
-
 def prime?(integer)
   if integer % 2 === 0 && integer != 2 || integer < 2
     return false
